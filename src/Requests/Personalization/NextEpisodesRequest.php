3ff40094fca7804e9ecf470ee98b1ef0939eed46
Rob Saurini <rob@saurini.com>
2015-10-26 11:34:27 -0700

    Add NextEpisodesRequest

---
+++ src/Requests/Personalization/NextEpisodesRequest.php
@@ -0,0 +1,25 @@
+<?php namespace DiscoveryDN\VidoraApiClient\Requests\Personalization;
+
+use DiscoveryDN\VidoraApiClient\Requests\Request;
+use DiscoveryDN\VidoraApiClient\Client;
+
+use Carbon\Carbon;
+
+class NextEpisodesRequest extends Request
+{
+    public function __construct(Client $client, $userId, Array $params = [])
+    {
+        $this->setClient($client);
+        $this->setPath('/v1/users/' . $userId . '/next_episodes');
+        $this->setMethod('GET');
+
+        // Add a default params
+        $this->addParams([
+            'api_key' => $client->getApiKey(),
+            'expires' => (new Carbon('UTC'))->addHours(1)->format('Y-m-d\TH:i'),
+        ]);
+
+        // Params added here will override the defaults
+        $this->addParams($params);
+    }
+}
