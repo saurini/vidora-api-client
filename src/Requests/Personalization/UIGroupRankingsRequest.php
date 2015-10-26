89970f22cc76979580b060c3788bd0ed63a6e5f5
Rob Saurini <rob@saurini.com>
2015-10-26 11:36:47 -0700

    Add UIGroupRankingsRequest

---
+++ src/Requests/Personalization/UIGroupRankingsRequest.php
@@ -0,0 +1,25 @@
+<?php namespace DiscoveryDN\VidoraApiClient\Requests\Personalization;
+
+use DiscoveryDN\VidoraApiClient\Requests\Request;
+use DiscoveryDN\VidoraApiClient\Client;
+
+use Carbon\Carbon;
+
+class UIGroupRankingsRequest extends Request
+{
+    public function __construct(Client $client, $userId, $groupId, Array $params = [])
+    {
+        $this->setClient($client);
+        $this->setPath('/v1/users/' . $userId . '/groups/' . $groupId . '/items');
+        $this->setMethod('GET');
+
+        // Add a default params
+        $this->addParams([
+            'api_key' => $client->getApiKey(),
+            'expires' => (new Carbon('UTC'))->addHours(1)->format('Y-m-d\TH:i'),
+        ]);
+
+        // Params added here will override the defaults
+        $this->addParams($params);
+    }
+}
