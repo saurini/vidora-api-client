2c0b98c8232c6b280eaeba316f8fde192c0cd4e7
Rob Saurini <rob@saurini.com>
2015-11-06 19:37:36 -0800

    Add test methods

--- spec/Requests/Behavioral/BehavioralRequestSpec.php
+++ spec/Requests/Behavioral/BehavioralRequestSpec.php
@@ -1,6 +1,7 @@
 <?php namespace spec\DiscoveryDN\VidoraApiClient\Requests\Behavioral;
 
 use DiscoveryDn\VidoraApiClient\Client;
+use DiscoveryDn\VidoraApiClient\Events\Event;
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
@@ -18,4 +19,25 @@ class BehavioralRequestSpec extends ObjectBehavior
     {
         $this->shouldHaveType('DiscoveryDN\VidoraApiClient\Requests\Behavioral\BehavioralRequest');
     }
+
+    function it_can_add_one_event()
+    {
+        $event = new Event(['user_id' => 12345, 'content_id' => 23456, 'type' => 'click']);
+
+        $this->addEvent($event)->shouldReturn($this);
+        $this->events->shouldHaveCount(1);
+    }
+
+    function it_can_add_many_events()
+    {
+        $event1 = new Event(['user_id' => 12345, 'content_id' => 23456, 'type' => 'click']);
+        $event2 = new Event(['user_id' => 13244, 'content_id' => 24523, 'type' => 'click']);
+        $event3 = new Event(['user_id' => 62422, 'content_id' => 24524, 'type' => 'click']);
+        $event4 = new Event(['user_id' => 24524, 'content_id' => 23433, 'type' => 'click']);
+
+        $this->addEvent($event1)->addEvent($event2)->addEvent($event3)->addEvent($event4)->shouldReturn($this);
+        $this->events->shouldHaveCount(4);
+    }
+
+
 }
